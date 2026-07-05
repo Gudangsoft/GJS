4e4bfa68f6350ae38d98638f70dbf11dd58a1746
Gudangsoft <gudangsoft.net@gmail.com>
2026-07-05 11:37:19 +0700

    feat: enhance dashboard quick actions for journal managers with additional options

--- resources/views/layouts/manager.blade.php
+++ resources/views/layouts/manager.blade.php
@@ -177,6 +177,22 @@ $activeJournal = $managedJournals->firstWhere("id", $activeJournalId) ?? $manage
         @if($activeJournal)
         @php
         $jId = $activeJournal->id;
+        $isManagerOrAdmin = $user->hasAnyRole(['super_admin', 'admin', 'journal_manager']);
+
+        $settingsGroup = [
+            ["label"=>"Seksi / Rubrik","icon"=>"M4 6h16M4 10h16M4 14h16M4 18h7","url"=>route("manager.sections"),"match"=>"manager/sections"],
+            ["label"=>"Pengumuman","icon"=>"M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z","url"=>route("manager.announcements"),"match"=>"manager/announcements"],
+            ["label"=>"Plugin Sidebar","icon"=>"M11 4a2 2 0 114 0v1a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-1a2 2 0 100 4h1a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-1a2 2 0 10-4 0v1a1 1 0 01-1 1H7a1 1 0 01-1-1v-3a1 1 0 00-1-1H4a2 2 0 110-4h1a1 1 0 001-1V7a1 1 0 011-1h3a1 1 0 001-1V4z","url"=>route("manager.plugins"),"match"=>"manager/plugins"],
+            ["label"=>"Menu Navigasi","icon"=>"M4 6h16M4 12h16M4 18h7","url"=>route("manager.menu"),"match"=>"manager/menu"],
+        ];
+        // Items below require journal_manager or higher role
+        if ($isManagerOrAdmin) {
+            array_unshift($settingsGroup,
+                ["label"=>"Profil & Pengaturan","icon"=>"M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z M15 12a3 3 0 11-6 0 3 3 0 016 0z","url"=>route("manager.settings"),"match"=>"manager/settings"]
+            );
+            $settingsGroup[] = ["label"=>"Halaman Jurnal","icon"=>"M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z","url"=>route("manager.pages"),"match"=>"manager/pages"];
+            $settingsGroup[] = ["label"=>"Import dari OJS","icon"=>"M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12","url"=>route("manager.ojs-import"),"match"=>"manager/ojs-import"];
+        }
 
         $navGroups = [
             "BERANDA" => [
@@ -191,15 +207,7 @@ $activeJournal = $managedJournals->firstWhere("id", $activeJournalId) ?? $manage
                 ["label"=>"Terbitan (Issue)","icon"=>"M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253","url"=>route("manager.issues"),"match"=>"manager/issues"],
                 ["label"=>"Letter of Acceptance","icon"=>"M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z","url"=>route("manager.loa"),"match"=>"manager/loa"],
             ],
-            "PENGATURAN JURNAL" => [
-                ["label"=>"Profil & Pengaturan","icon"=>"M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z M15 12a3 3 0 11-6 0 3 3 0 016 0z","url"=>route("manager.settings"),"match"=>"manager/settings"],
-                ["label"=>"Seksi / Rubrik","icon"=>"M4 6h16M4 10h16M4 14h16M4 18h7","url"=>route("manager.sections"),"match"=>"manager/sections"],
-                ["label"=>"Pengumuman","icon"=>"M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z","url"=>route("manager.announcements"),"match"=>"manager/announcements"],
-                ["label"=>"Plugin Sidebar","icon"=>"M11 4a2 2 0 114 0v1a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-1a2 2 0 100 4h1a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-1a2 2 0 10-4 0v1a1 1 0 01-1 1H7a1 1 0 01-1-1v-3a1 1 0 00-1-1H4a2 2 0 110-4h1a1 1 0 001-1V7a1 1 0 011-1h3a1 1 0 001-1V4z","url"=>route("manager.plugins"),"match"=>"manager/plugins"],
-                ["label"=>"Halaman Jurnal","icon"=>"M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z","url"=>route("manager.pages"),"match"=>"manager/pages"],
-                ["label"=>"Menu Navigasi","icon"=>"M4 6h16M4 12h16M4 18h7","url"=>route("manager.menu"),"match"=>"manager/menu"],
-                ["label"=>"Import dari OJS","icon"=>"M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12","url"=>route("manager.ojs-import"),"match"=>"manager/ojs-import"],
-            ],
+            "PENGATURAN JURNAL" => $settingsGroup,
             "PENGGUNA" => [
                 ["label"=>"Daftar Pengguna","icon"=>"M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z","url"=>route("manager.users"),"match"=>"manager/users"],
             ],
